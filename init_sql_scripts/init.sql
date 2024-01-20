c5f41154b58ad1746bb1342c6e8abfb4ffe269fb
Stephan Lamoureux <sa.lamoureux@gmail.com>
2024-01-20 14:29:40 -0500

    added entry

--- init_sql_scripts/init.sql
+++ init_sql_scripts/init.sql
@@ -7,3 +7,4 @@ insert into movie_hero(movie, hero) values ('Titanic', 'Leonardo DiCaprio');
 insert into movie_hero(movie, hero) values ('The GodFather', 'Marlon Brando');
 insert into movie_hero(movie, hero) values ('Inception', 'Leonardo DiCaprio');
 insert into movie_hero(movie, hero) values ('Russia', 'Moscow');
+insert into movie_hero(movie, hero) values ('Russia', 'Moscow');
